622c2ea5bb7055440420f1c75f481f84c2f1d9e7
MystRiim <157691471+MystRiim@users.noreply.github.com>
2025-09-05 11:28:33 +0200

    update MVC

--- script.js
+++ script.js
@@ -2,41 +2,41 @@
 let chooseName;
 let chooseSub;
 let choosePlace;
-let app = document.getElementById("app");
 
 // view
 updateView();
 function updateView() {
-  app.innerHTML = /* html */ `
-    <div class="main-container">
-      <h1>Story time</h1>
-      <div class="text-container">
-          <div class="text">Once there was a</div>
-          <div class="text" id="firstWord">__</div>
-          <div class="text">that took a</div>
-          <div class="text" id="secWord">__</div>
-          <div class="text">and shared it with others in the</div>
-          <div class="text" id="thirdWord">__</div>
-          <div class="text"> - everyone was then happy. The End.</div>
-      </div>
-      <br>
-      <div class="btn-container">
-        <button onclick="changeName(this.innerHTML)">Panda</button>
-        <button onclick="changeName(this.innerHTML)">Cat</button>
-        <button onclick="changeName(this.innerHTML)">Dog</button>
-      </div>
-      <br>
-      <div class="btn-container">
-        <button onclick="changeSub(this.innerHTML)">cake</button>
-        <button onclick="changeSub(this.innerHTML)">cloud</button>
-      </div>
-      <br>
-      <div class="btn-container">
-        <button onclick="changePlace(this.innerHTML)">pool</button>
-        <button onclick="changePlace(this.innerHTML)">forest</button>
-      </div>
+  let html = /* html */ `
+  <div class="main-container">
+    <h1>Story time</h1>
+    <div class="text-container">
+        <div class="text">Once there was a</div>
+        <div class="text" id="firstWord">__</div>
+        <div class="text">that took a</div>
+        <div class="text" id="secWord">__</div>
+        <div class="text">and shared it with others in the</div>
+        <div class="text" id="thirdWord">__</div>
+        <div class="text"> - everyone was then happy. The End.</div>
     </div>
-  `;
+    <br>
+    <div class="btn-container">
+      <button onclick="changeName(this.innerHTML)">Panda</button>
+      <button onclick="changeName(this.innerHTML)">Cat</button>
+      <button onclick="changeName(this.innerHTML)">Dog</button>
+    </div>
+    <br>
+    <div class="btn-container">
+      <button onclick="changeSub(this.innerHTML)">cake</button>
+      <button onclick="changeSub(this.innerHTML)">cloud</button>
+    </div>
+    <br>
+    <div class="btn-container">
+      <button onclick="changePlace(this.innerHTML)">pool</button>
+      <button onclick="changePlace(this.innerHTML)">forest</button>
+    </div>
+  </div>
+`;
+  document.getElementById("app").innerHTML = html;
 }
 
 // controller
